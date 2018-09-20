d2965e5963483ccfe620987dd0568d047fff556e
Alexa Moy <s.alexa.moy@gmail.com>
2018-09-20 17:25:16 -0400

    Initialized project with create-react-app

---
+++ src/firestore/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyCF-sriJyl4UeGXA2ZAaiojp1fM5AQBFY0',
+  authDomain: 'list-of-things-7a1bb.firebaseapp.com',
+  databaseURL: 'https://list-of-things-7a1bb.firebaseio.com',
+  projectId: 'list-of-things-7a1bb',
+  storageBucket: 'list-of-things-7a1bb.appspot.com',
+  messagingSenderId: '1028695312680'
+};
+
+export default config;
---
+++ src/firestore/index.js
@@ -0,0 +1,11 @@
+import config from './config';
+const firebase = require('firebase');
+require('firebase/firestore');
+require('firebase/auth');
+
+firebase.initializeApp(config);
+const db = firebase.firestore();
+const settings = { timestampsInSnapshots: true };
+db.settings(settings);
+
+export default db;
